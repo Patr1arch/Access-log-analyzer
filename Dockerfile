880e385a46617da031eee313017febb388379c95
Nikita <nikitabaranov200021@gmail.com>
2021-11-03 19:38:01 +1000

    Add implementation of the main algorithm

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,6 @@ COPY analyzer.php .
 WORKDIR .
 RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer 
 RUN composer init --require=kassner/log-parser:~2.0
+RUN docker-php-ext-install bcmath
 RUN apt-get -y update && apt-get -y install git && composer update
 
--- analyzer.php
+++ analyzer.php
@@ -1,7 +1,7 @@
 <?php
 include_once(__DIR__.'/vendor/autoload.php');
 use Kassner\LogParser\LogParser;
-
+date_default_timezone_set('Asia/Vladivostok');
 $patterns = [
         '/\/01\//', '/\/02\//', '/\/03\//', '/\/04\//',
         '/\/05\//', '/\/06\//', '/\/07\//', '/\/08\//',
@@ -22,28 +22,52 @@ function formatDate(&$pt, &$rep, &$str){
 
 // TODO: Test some command inputs
 $options = getopt("u:t:");
-var_dump($options);
-if (empty($options['u']) || empty($options['t']))
+if (!isset($options['u']) || !isset($options['t']))
         throw new Exception("Required command parameters are not set");
-if (!preg_match('/\d+?(.\d*)/', $options['u']))
+if (strval(floatval($options['u'])) != $options['u'])
         throw new Exception("Required command parameter for -u is not a number");
-if (!preg_match('/\d+?(.\d*)/', $options['t']))
+if (strval(floatval($options['t'])) != $options['t'])
         throw new Exception("Required command parameter for -t is not a number");
 $parser = new LogParser();
 $parser->addPattern('%c', '(?P<caller>@[\w?-]+|-)');
 $parser->addPattern('%g', '(?P<priority>[a-zA-Z]+\:\d+)');
 $parser->setFormat('%h %l %u %t "%r" %>s %b %T "%{Referer}i" "%c" %g');
+$startTime = null;
+$endTime = null;
+$currentAccess = 0;
+$count = 0;
+$notFailCount = 0;
 while(!feof(STDIN)){ // TODO: Empty input?
 	$line = fgets(STDIN);
 	if ($line == '' || ctype_space($line)) 
 		continue;
         $fLine = formatDate($patterns, $replacements, $line);
-        var_dump($fLine);
         $entry = $parser->parse($fLine);
-        $entry->isFail = floatval($entry->requestTime) > floatval($options['t'])
-                || !preg_match('/[^5]\d{2}|-/', $entry->status); // Warn: float comparing
-        var_dump($entry);
+        $entry->isFail = bccomp(floatval($entry->requestTime), floatval($options['t']), 6) > 0
+                || !preg_match('/[^5]\d{2}|-/', $entry->status);
+	if (!isset($startTime)) {
+		if ($entry->isFail) {
+			$notFailCount = 0;
+			$startTime = $entry->stamp;
+			$endTime = $startTime;
+			$currentAccess = 0;
+			$count = 1;
+		}
+		continue;
+	}
+	else if (!$entry->isFail)
+		$notFailCount++;
+	$currentAccessK = (floatval($notFailCount) / floatval(++$count)) * 100;
+	if (bccomp(floatval($currentAccessK), floatval($options['u']), 6) < 0) {
+		$currentAccess = $currentAccessK;
+		$endTime = $entry->stamp;
+	}
+	else {
+		echo date("H:i:s", $startTime)."\t".date("H:i:s", $endTime)."\t".number_format($currentAccess, 2)."\n";
+		$startTime = null;
+	}
 }
-
+if (isset($startTime) && bccomp(floatval($currentAccess), floatval($options['u']), 6) <= 0)
+	echo date("H:i:s", $startTime)."\t".date("H:i:s", $endTime)."\t".number_format($currentAccess, 2)."\n";
 ?>
 
